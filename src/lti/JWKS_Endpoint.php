d56ba021de0c5b0f52ae5bfb33dd9694c4eeb730
Damian <damian@tiltroleplay.com>
2026-06-01 16:16:31 +0100

    removing logging

--- src/lti/JWKS_Endpoint.php
+++ src/lti/JWKS_Endpoint.php
@@ -1,55 +1,68 @@
 <?php
 namespace IMSGlobal\LTI;
 
-use phpseclib\Crypt\RSA;
-use \Firebase\JWT\JWT;
+use phpseclib3\Crypt\RSA;
+use phpseclib3\Crypt\PublicKeyLoader;
+use Firebase\JWT\JWT;
 
 class JWKS_Endpoint {
 
-    private $keys;
+    private array $keys;
 
     public function __construct(array $keys) {
         $this->keys = $keys;
     }
 
-    public static function new($keys) {
+    public static function new(array $keys): self {
         return new JWKS_Endpoint($keys);
     }
 
-    public static function from_issuer(Database $database, $issuer) {
+    public static function from_issuer(Database $database, string $issuer): self {
         $registration = $database->find_registration_by_issuer($issuer);
         return new JWKS_Endpoint([$registration->get_kid() => $registration->get_tool_private_key()]);
     }
 
-    public static function from_registration(LTI_Registration $registration) {
+    public static function from_registration(LTI_Registration $registration): self {
         return new JWKS_Endpoint([$registration->get_kid() => $registration->get_tool_private_key()]);
     }
 
-    public function get_public_jwks() {
+    public function get_public_jwks(): array {
         $jwks = [];
         foreach ($this->keys as $kid => $private_key) {
-            $key = new RSA();
-            $key->setHash("sha256");
-            $key->loadKey($private_key);
-            $key->setPublicKey(false, RSA::PUBLIC_FORMAT_PKCS8);
-            if ( !$key->publicExponent ) {
+            try {
+                // Load the private key using phpseclib3
+                $key = PublicKeyLoader::load($private_key);
+
+                // Get the public key
+                $publicKey = $key->getPublicKey();
+
+                // Convert to array format to extract n and e
+                $keyDetails = openssl_pkey_get_details(openssl_pkey_get_public($publicKey->toString('PKCS8')));
+
+                if (!isset($keyDetails['rsa'])) {
+                    continue;
+                }
+
+                $components = [
+                    'kty' => 'RSA',
+                    'alg' => 'RS256',
+                    'use' => 'sig',
+                    'e' => JWT::urlsafeB64Encode($keyDetails['rsa']['e']),
+                    'n' => JWT::urlsafeB64Encode($keyDetails['rsa']['n']),
+                    'kid' => $kid,
+                ];
+
+                $jwks[] = $components;
+            } catch (\Exception $e) {
+                error_log("[LTI JWKS] Failed to process key: " . $e->getMessage());
                 continue;
             }
-            $components = array(
-                'kty' => 'RSA',
-                'alg' => 'RS256',
-                'use' => 'sig',
-                'e' => JWT::urlsafeB64Encode($key->publicExponent->toBytes()),
-                'n' => JWT::urlsafeB64Encode($key->modulus->toBytes()),
-                'kid' => $kid,
-            );
-            
-            $jwks[] = $components;
         }
         return ['keys' => $jwks];
     }
 
-    public function output_jwks() {
+    public function output_jwks(): void {
+        header('Content-Type: application/json');
         echo json_encode($this->get_public_jwks());
     }
 
